97cbb12d822348b331b7103e2de099dbc62a8f09
AmrMansour94 <amr.mansour6644@gmail.com>
2022-08-19 18:49:38 +0200

    Add Radio Hover Css File

--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,7 @@ namespace ChicksAppNew
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "wwwroot/build";
+                configuration.RootPath = "ClientApp/build";
             });
 
             // Adjust Json Return Values To Use Same Format As ViewModels
